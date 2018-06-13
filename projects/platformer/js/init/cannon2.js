cb5eeeedb9f19ca093315f384b34128a46ac107e
Shawn Fremin <sfremin2@gmail.com>
2018-06-13 16:15:09 +0000

    updating code

---
+++ projects/platformer/js/init/cannon2.js
@@ -0,0 +1,31 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary (at least 3) to make your level challenging. 
+     *
+     * The following functions are available to you:
+     *  cannon.create.onTop(xLocation);
+     *  cannon.create.onBottom(xLocation);
+     *  cannon.create.onLeft(yLocation);
+     *  cannon.create.onRight(yLocation);
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        //cannon.create.onLeft(165, 7000); //cannon left 1
+        //cannon.create.onRight(305, 1000); //cannon right 1
+        //cannon.create.onLeft(450, 0); //cannon left 2
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
